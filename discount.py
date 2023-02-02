cff5a5460a150d7b30492deb0027363ad75d7d68
Mesavictor <106388517+Mesavictor@users.noreply.github.com>
2023-02-02 16:58:59 +0300

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,5 +1,5 @@
-a=float(input("Enter the amount of purchase "))
+a=float(input("Enter the amount of purchase: "))
 if a>1000:
-    print("Discount=",0.05*a)
+    print("Discount=sh",0.05*a)
 else:
     print("No discount ")
